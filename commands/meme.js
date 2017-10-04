c2425892ee24a27cdc022fca070afe27f6a7d624
raphael <raph.koh@gmail.com>
2017-10-03 23:41:00 -0400

    Coverted rest of legacy commands and moved them to commands

---
+++ commands/meme.js
@@ -0,0 +1,8 @@
+module.exports = function (bot) {
+  bot.registerCommand('meme', (msg, args) => {
+    var topword = args[0]
+    var bottomword = args[1]
+    var url = args[2]
+    bot.createMessage(msg.channel.id, `https://memegen.link/custom/${topword}/${bottomword}.jpg?alt=${url}?font=impact`)
+  })
+}
--- legacy_commands/meme.js
+++
@@ -1,9 +0,0 @@
-const logger = require("../utils/logger.js");
-module.exports = function(bot) {
-	bot.registerCommand("meme", (msg, args) => {
-		var topword = args[0];
-		var bottomword = args[1];
-		var url = args[2];
-		bot.createMessage(msg.channel.id, `https://memegen.link/custom/${topword}/${bottomword}.jpg?alt=${url}?font=impact`);
-	});
-};
